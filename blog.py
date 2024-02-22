9461e8c4e58d3908d56ceff20ffd73c911d5ddd9
PythonShinobi <pythonshinobi.com>
2024-02-22 20:24:05 +0300

    Add script to run Flask application.

---
+++ blog.py
@@ -0,0 +1,11 @@
+
+# Import the create_app function from the app module
+from app import create_app
+
+# Create the Flask application using the create_app function
+flask_app = create_app()
+
+# Check if the script is being run directly
+if __name__ == '__main__':
+    # Run the Flask application on port 5000
+    flask_app.runt(port=5000)
