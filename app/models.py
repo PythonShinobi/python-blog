5dfce5bc2c5fdca394a08c9cd7993e53750f864f
PythonShinobi <pythonshinobi.com>
2024-02-23 11:51:18 +0300

    Add UserMixin to the user model and define a way to load a user from the database.

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 
 # Import necessary modules.
+from flask_login import UserMixin
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String
 
 # Import the SQLAlchemy instance from the app module.
-from app import db
+from app import db, login_manager
 
 # Define the User model class.
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     # Define columns with their types and properties.
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -18,3 +19,7 @@ class User(db.Model):
     # Define a representation method for easier debugging.
     def __repr__(self):
         return f'<User {self.username}>'  # Return a string representation of the User object.
+    
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
