e6690f52c544c6357b76309c3ac14d6365685f05
PythonShinobi <pythonshinobi.com>
2024-02-26 13:41:54 +0300

    Update variable names for the admin email and password and add a new variable name 'ADMIN'.

--- config.py
+++ config.py
@@ -16,7 +16,8 @@ class Config:
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS') is not None
-    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
-    ADMIN_EMAIL_PASSWORD = os.getenv('ADMIN_EMAIL_PASSWORD')
+    MAIL_USERNAME = os.getenv('ADMIN_EMAIL')
+    MAIL_PASSWORD = os.getenv('ADMIN_EMAIL_PASSWORD')
     EMAIL_NOTIFICATION_ERROR = os.getenv('ERROR_NOTIFICATION_EMAIL')
     EMAIL_NOTIFICATION_ERROR_PASSWORD = os.getenv('ERROR_NOTIFICATION_EMAIL_PASSWORD')
+    ADMIN = ['pythonshinobi@gmail.com']
