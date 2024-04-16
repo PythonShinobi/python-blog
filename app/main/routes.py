f57b531357970bb9dc48092a76ac399acb929a18
PythonShinobi <pythonshinobi@gmail.com>
2024-04-16 23:03:20 +0300

    Refactored comment submission redirection to 'auth.login' route for unauthenticated users

--- app/main/routes.py
+++ app/main/routes.py
@@ -91,7 +91,7 @@ def show_post(post_id):
     if comment_form.validate_on_submit():
         if not current_user.is_authenticated:
             flash('You need to login to comment')
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
         
         new_comment = Comment(
             text=comment_form.comment_text.data,
